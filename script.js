88f310ec976152ca5bf24c578935516d04784a0d
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-04-01 11:39:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var fontsize = document.getElementById("fontsize")
+var fontcolor = document.getElementById("fontcolor")
+
+var myCookies = document.cookeie.split(";")
+
+myCookies.map((item) => {
+	var cookielist = item.split("=")
+})
+function onSubmit(event){
+	document.cookies = `fontsize = ${fontsize.value}`
+	document.cookies = `fontcolor = ${fontcolor.value}`
+	event.preventDefault();
+}
